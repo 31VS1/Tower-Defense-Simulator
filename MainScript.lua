a6f68215d266cce348b03806625da78a1b148963
31VS1 <121323408+31VS1@users.noreply.github.com>
2023-07-11 03:13:33 -0500

    Create MainScript.lua

---
+++ MainScript.lua
@@ -0,0 +1,7 @@
+repeat task.wait() until game:IsLoaded()
+local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or function() end
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.Started then
+        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/31VS1/rektsky4roblox/main/mainscript.lua'))()")
+    end
+end)
