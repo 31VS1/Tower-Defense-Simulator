6d59a8c850975a1625e13c4b36360cbcc0fa9824
31VS1 <121323408+31VS1@users.noreply.github.com>
2023-07-11 03:31:01 -0500

    fix :(*

--- Script.lua
+++ Script.lua
@@ -3,17 +3,13 @@ local teleportService = game:GetService("TeleportService")
 local runService = game:GetService("RunService")
 
 local function checkTeleport()
-    local results = game:GetService("StarterGui").GameGui.Results
+    local resultsGui = player.PlayerGui:FindFirstChild("Results")
 
-    if results and player:FindFirstChild("PlayerGui") then
-        local resultsGui = player.PlayerGui:FindFirstChild("Results")
-        if resultsGui and resultsGui:IsA("ScreenGui") and resultsGui.Enabled then
-            teleportService:Teleport(3260590327, player)
-            return
-        end
+    if not resultsGui or not resultsGui:IsA("ScreenGui") or not resultsGui.Enabled then
+        teleportService:Teleport(3260590327, player)
     end
 end
 
-runService.Stepped:Connect(function()
+runService.RenderStepped:Connect(function()
     checkTeleport()
 end)
