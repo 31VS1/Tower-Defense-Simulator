11187dbf1a4f18f7c81d66ccd87d73b3db910a61
31VS1 <121323408+31VS1@users.noreply.github.com>
2023-07-11 03:14:04 -0500

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -2,6 +2,6 @@ repeat task.wait() until game:IsLoaded()
 local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or function() end
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
-        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/31VS1/rektsky4roblox/main/mainscript.lua'))()")
+        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/31VS1/Tower-Defense-Simulator/main/Script.lua'))()")
     end
 end)
